ad91d960092f2c6cc8cda6f5f958447767061ff1
Sebatian Klüft <skluft@gmail.com>
2016-02-18 23:58:01 +0100

    Form for creating grouops

---
+++ src/clojurescript/skuld/create_group.cljs
@@ -0,0 +1,5 @@
+(ns skuld.create-group)
+
+(defn create-group-form []
+  [:input {:type "text"
+           :value "woo"}])
---
+++ src/clojurescript/skuld/web_gui.cljs
@@ -0,0 +1,46 @@
+(ns skuld.web-gui
+  (:require [reagent.core :as r]
+            [clojure.string :as s]))
+
+(defonce value (r/atom "foo"))
+
+(defn get-element [id]
+  (.getElementById js/document id))
+
+(defonce group-members (r/atom ["John" "scott"]))
+
+(defn print-group-members [members]
+  [:pre (s/join "\n" (map-indexed #(str %1 ": " %2) members))])
+
+(defn group-member-form [members]
+  (into [:div]
+        (map-indexed
+          (fn [idx member-name]
+            [:div
+             [:input {:type "text"
+                      :value member-name
+                      :on-change #(swap! members assoc idx (-> % .-target .-value))}]])
+          @members)))
+
+(defn create-group-form []
+  [:div {:class "container"}
+   [:form
+    [:p "Group name:"]
+    [:input {:type "text"}]
+    [:p "Input names of group members:"]
+    [group-member-form group-members]]
+   [:button {:type "button"
+             :on-click #(swap! group-members conj "")}
+    "+1 group member"]
+   [:button {:type "button"
+             :on-click #(swap! group-members (comp vec drop-last))}
+    "-1 group member"]
+   [print-group-members @group-members]])
+
+(defn init-start []
+  (r/render-component [create-group-form] (get-element "app")))
+
+; Some routing (sort of)
+(condp re-matches (-> js/window .-location .-pathname)
+  #"/" (init-start))
+
