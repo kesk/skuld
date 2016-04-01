0bb20b42f041f36cb100966df807d76f7506f813
Sebatian Klüft <skluft@gmail.com>
2016-04-02 01:10:42 +0200

    Rename rest api

--- src/clojure/skuld/core.clj
+++ src/clojure/skuld/core.clj
@@ -1,28 +1,29 @@
 (ns skuld.core
-  (:require [clojure.tools.logging :as log]
+  (:require [clojure.string :as s]
+            [clojure.tools.logging :as log]
             [compojure.core :refer [GET POST context defroutes]]
             [compojure.route :as route]
             [ring.adapter.jetty :refer [run-jetty]]
             [ring.middleware.params :refer [wrap-params]]
             [ring.middleware.session :refer [wrap-session]]
             [ring.util.response :as response]
-            [skuld.api.api-router :refer [api-handler]]
+            [skuld.rest-api :refer [api-routes]]
             [skuld.data-model :as data])
   (:gen-class))
 
 (defn handle-group-request
   [req]
-  (let [group-id (data/create-group (get-in req [:params "group_name"]))]
-    (data/create-user (get-in req [:params "member0"]) group-id)
+  (let [members (for [[k v] (:params req)
+                      :when (and (not (s/blank? v))
+                                 (re-matches #"member\d+" k))] (s/trim v))
+        group-id (data/create-group (get-in req [:params "group_name"]) members)]
     (response/redirect (str "/groups/" group-id))))
 
 (defroutes app-routes
   (GET "/" [] (response/resource-response "create_group.html"))
-  (GET "/groups" [] handle-group-request)
   (GET "/groups/:group-id" [group-id] (response/resource-response "groups.html"))
   (POST "/groups" [] handle-group-request)
-  (GET "/redirect" [] (response/redirect "/group"))
-  (context "/api/v1" [] api-handler)
+  (context "/api/v1" [] api-routes)
   (GET "/hello/:n" [n] (str "Hello " n "!"))
   (route/resources "/"))
 
--- src/clojure/skuld/rest_api.clj
+++ src/clojure/skuld/rest_api.clj
@@ -1,4 +1,4 @@
-(ns skuld.api.api-router
+(ns skuld.rest-api
   (:require [clojure.data.json :as json]
             [compojure.core :refer [ANY defroutes]]
             [liberator.core :refer [defresource]]
@@ -10,9 +10,14 @@
   :handle-ok (fn [ctx]
                (json/write-str (groups/get-group id))))
 
+(defresource expenses-resourse [group-id]
+  :available-media-types ["application/json"]
+  :handle-ok (fn [ctx] "EXPENSES"))
+
 (defn- hello-world [request]
   (-> (res/response "Hello, api!")
       (res/content-type "*/*")))
 
-(defroutes api-handler
-  (ANY "/groups/:id" [id] (groups-resource id)))
+(defroutes api-routes
+  (ANY "/groups/:id" [id] (groups-resource id))
+  (ANY "/groups/:id/expenses" [id] (expenses-resourse id)))
