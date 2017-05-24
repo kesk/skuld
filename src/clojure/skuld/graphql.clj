984a0ed754e0a989a9e3cc2a6363ddb499aa6de7
Sebastian Klüft <sebastian.kluft@pagero.com>
2017-05-24 19:21:14 +0200

    Some more GraphQL testing

--- src/clojure/skuld/graphql.clj
+++ src/clojure/skuld/graphql.clj
@@ -26,19 +26,30 @@
 (defrecord GroupMembers [id]
   data/Resolvable
   (resolve! [_ _]
-    (get-group-members-query {:group_id id}))
+    (get-group-members-query {:id id}))
 
   data/Transform
   (transform [_ members]
-    (map :name members)))
+     (map :name members)))
+
+(defrecord GroupExpenses [id]
+  data/Resolvable
+  (resolve! [_ _]
+    (get-group-expenses-query {:id id} query-conf)))
 
 (defrecord Group [id]
   data/Resolvable
   (resolve! [_ _]
     (d/future
-      (-> (get-group-query {:id id})
-          (first)
-          (assoc :members (->GroupMembers id))))))
+      (first (get-group-query {:id id} query-conf))))
+
+  data/Transform
+  (transform [_ group]
+    (some-> group
+            (assoc :members (->GroupMembers id))
+            (assoc :expenses (->GroupExpenses id)))))
 
 (engine/run!! (->Group "351ca88f-31cd-44ea-b077-1b80e0a4cb89"))
+(engine/run!! (->Group "8fa524d4-f01d-4db7-8488-d4851b34466a"))
+(engine/run!! (->GroupExpenses "8fa524d4-f01d-4db7-8488-d4851b34466a"))
 (engine/run! (->GroupMembers "351ca88f-31cd-44ea-b077-1b80e0a4cb89"))
--- src/clojure/skuld/graphql.sql
+++ src/clojure/skuld/graphql.sql
@@ -2,4 +2,7 @@
 SELECT * FROM groups WHERE id = :id;
 
 -- name: get-group-members-query
-SELECT name FROM users WHERE group_id = :group_id;
+SELECT name FROM users WHERE group_id = :id;
+
+-- name: get-group-expenses-query
+SELECT id, payed_by, amount, date FROM expenses WHERE group_id = :id;
