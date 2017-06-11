992164058deae41d6bae7c8114512fd471fadd00
Sebatian Klüft <skluft@gmail.com>
2017-06-11 22:15:45 +0200

    Added Expenses to GraphQL

--- src/clojure/skuld/graphql.clj
+++ src/clojure/skuld/graphql.clj
@@ -1,32 +1,39 @@
 (ns skuld.graphql
   (:require [camel-snake-kebab.core :refer [->kebab-case]]
             [claro.data :as data]
-            [claro.engine :as engine]
             [environ.core :refer [env]]
             [manifold.deferred :as d]
-            [yesql.core :refer [defqueries]]))
+            [yesql.core :refer [defqueries]]
+            [alumbra.core :as alumbra]
+            [clojure.pprint :refer [pprint]]))
 
 (def db-spec {:classname "org.sqlite.JDBC"
               :subprotocol "sqlite"
               :subname (env :database-url)
               :foreign_keys "on"})
 
-(def query-conf {:connection db-spec
-                 :identifiers ->kebab-case})
+(def query-conf {:connection db-spec})
 
 (defqueries "skuld/graphql.sql" query-conf)
 
 (def schema
-  "type Group { id: ID!, name: String!, members: [String!]! }
-   type QueryRoot { group(id: ID!): Group }
-   schema { query: QueryRoot }")
+  "type Expense { id: ID!, payed_by: String!, amount: Float!, data_added: String! }
+  type Dept { id: ID!, user_name: String!, amount: Float!, expense: Expense! }
+  type Group { id: ID!, name: String!, members: [String!]!, expenses: [Expense!]! }
+  type QueryRoot { group(id: ID!): Group }
+  schema { query: QueryRoot }")
 
-(get-group-query {:id "351ca88f-31cd-44ea-b077-1b80e0a4cb89"})
+(defrecord Expense [id]
+  data/Resolvable
+  data/BatchedResolvable
+  (resolve-batch! [_ _ expenses]
+    (d/future
+      (get-expenses-query {:ids (map :id expenses)} query-conf))))
 
 (defrecord GroupMembers [id]
   data/Resolvable
   (resolve! [_ _]
-    (get-group-members-query {:id id}))
+    (get-group-members-query {:id id} query-conf))
 
   data/Transform
   (transform [_ members]
@@ -37,35 +44,18 @@
   (resolve! [_ _]
     (get-group-expenses-query {:id id} query-conf)))
 
-(defn- mk-dept-pair [{:keys [owed-by owed-to amount]}]
-  (let [pair-dept (if (< 0 (compare owed-by owed-to)) (* -1 amount) amount)]
-    [(sorted-set owed-by owed-to) pair-dept]))
-
-(defn- merge-dept-pairs
-  "Merge dept pairs to get a sum of the dept between two users."
-  [pairs]
-  (let [f (fn [p [k v]] (update p k (fnil + 0) v))]
-    (reduce f {} pairs)))
-
-(defn- format-dept [[pair amount]]
-  (let [owed-by (if (< 0 amount) (first pair) (second pair))
-        owed-to (if (< 0 amount) (second pair) (first pair))]
-    {:owed-by owed-by
-     :owed-to owed-to
-     :amount (if (< 0 amount) amount (* -1 amount))}))
-
 (defrecord GroupDept [id]
   data/Resolvable
   (resolve! [_ _]
     (get-group-dept-query {:id id} query-conf))
 
   data/Transform
-  (transform [_ dept]
-    (->> dept
-        (map mk-dept-pair)
-        merge-dept-pairs
-        (filter #(not= (second %) 0.0))
-        (map format-dept))))
+  (transform [_ depts]
+    (let [insert-expense (fn [dept]
+                           (-> dept
+                               (assoc :expense (->Expense (:expense_id dept)))
+                               (dissoc :expense_id)))]
+      (map insert-expense depts))))
 
 (defrecord Group [id]
   data/Resolvable
@@ -77,9 +67,19 @@
   (transform [_ group]
     (some-> group
             (assoc :members (->GroupMembers id))
-            (assoc :expenses (->GroupExpenses id)))))
+            (assoc :expenses (->GroupExpenses id))
+            (assoc :dept (->GroupDept id)))))
 
-(engine/run!! (->Group "351ca88f-31cd-44ea-b077-1b80e0a4cb89"))
-(engine/run!! (->GroupExpenses "351ca88f-31cd-44ea-b077-1b80e0a4cb89"))
-(engine/run!! (->GroupDept "351ca88f-31cd-44ea-b077-1b80e0a4cb89"))
-(engine/run! (->GroupMembers "351ca88f-31cd-44ea-b077-1b80e0a4cb89"))
+(def QueryRoot
+  {:group (map->Group {})})
+
+(def graphql-handler
+  (alumbra/handler
+    {:schema schema
+     :query QueryRoot}))
+
+#_(pprint (engine/run!! (->Group "351ca88f-31cd-44ea-b077-1b80e0a4cb89")))
+#_(engine/run!! (->GroupExpenses "351ca88f-31cd-44ea-b077-1b80e0a4cb89"))
+#_(engine/run!! (->GroupDept "351ca88f-31cd-44ea-b077-1b80e0a4cb89"))
+#_(engine/run!! (->GroupMembers "351ca88f-31cd-44ea-b077-1b80e0a4cb89"))
+#_(engine/run!! (->Expense 1))
--- src/clojure/skuld/graphql.sql
+++ src/clojure/skuld/graphql.sql
@@ -7,12 +7,11 @@ SELECT name FROM users WHERE group_id = :id;
 -- name: get-group-expenses-query
 SELECT id, payed_by, amount, date AS date_added FROM expenses WHERE group_id = :id;
 
+-- name: get-expenses-query
+SELECT * FROM expenses WHERE id IN (:ids);
+
 -- name: get-group-dept-query
-SELECT
-d.user_name AS owed_by,
-e.payed_by AS owed_to,
-SUM(d.amount) AS amount
-FROM dept AS d
-JOIN expenses AS e ON d.expense_id = e.id
-WHERE d.group_id = :id
-GROUP BY owed_by, owed_to;
+SELECT id, user_name, expense_id, SUM(amount) AS amount
+FROM dept
+WHERE group_id = :id
+GROUP BY user_name;
